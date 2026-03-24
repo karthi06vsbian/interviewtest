b3545ddc57168ad94b7b705419f5a65b1e423b77
KarthiKeyan A <karthi06vsbian@gmail.com>
2026-03-24 21:15:42 +0530

    Configure WhiteNoise and update STATIC_URL
    Added WhiteNoise middleware and updated static file settings.

--- ai_interview/settings.py
+++ ai_interview/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ai_interview.urls'
@@ -131,7 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (uploaded resumes)
 MEDIA_URL = '/media/'
